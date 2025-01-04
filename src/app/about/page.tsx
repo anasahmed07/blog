ac92f169e8ca7e3eb44b962ced272ee93c25274f
anasahmedshaikh <anasahmedshaikh.work@gmail.com>
2025-01-05 00:50:02 +0500

    &apos; errors

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -40,16 +40,16 @@ import Link from 'next/link';
 export default function AboutPage() {
 
   return (
-    <div className="flex flex-col justify-center items-start max-w-2xl mx-auto my-10 w-full">
+    <div className="px-6 flex flex-col justify-center items-start max-w-2xl mx-auto my-10 w-full">
       <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
         About Me
       </h1>
       <div className="flex flex-col gap-4 mb-8 prose dark:prose-dark leading-6 max-w-2xl">
-        <h6>Hey there! I'm <strong>Anas Ahmed Shaikh</strong>. I'm a software engineer currently living in <strong>Karachi, Pakistan.</strong></h6>
-        <p>I grew up surrounded by technology and always had a passion for building things on the web. I studied programming and quickly fell in love with frontend development, especially using Next.js and Sanity CMS to create modern, dynamic websites and applications ✨. I’m a big fan of TypeScript because it helps me build scalable and maintainable codebases.</p>
-        <p>Before diving into web development, I wanted to create designs and visuals, but I found that I enjoyed building interactive and functional websites even more. That’s why I now focus on building portfolio websites and e-commerce stores that combine great design with seamless performance 👨‍💻.</p>
+        <h6>Hey there! I&apos;m <strong>Anas Ahmed Shaikh</strong>. I&apos;m a software engineer currently living in <strong>Karachi, Pakistan.</strong></h6>
+        <p>I grew up surrounded by technology and always had a passion for building things on the web. I studied programming and quickly fell in love with frontend development, especially using Next.js and Sanity CMS to create modern, dynamic websites and applications ✨. I&apos;m a big fan of TypeScript because it helps me build scalable and maintainable codebases.</p>
+        <p>Before diving into web development, I wanted to create designs and visuals, but I found that I enjoyed building interactive and functional websites even more. That&apos;s why I now focus on building portfolio websites and e-commerce stores that combine great design with seamless performance 👨‍💻.</p>
         <p>In my free time, I love being outdoors, reading comics and books, and watching movies.</p>
-        <p>If you want to learn more about me, check out my resume, timeline and please don't hesitate to contact me!</p>
+        <p>If you want to learn more about me, check out my resume, timeline and please don&apos;t hesitate to contact me!</p>
         <Link href={"#"} target='blank'>
           <button className="flex items-center justify-center right-1 top-1 px-4 font-medium h-8 bg-primary dark:bg-secondary text-gray-100 rounded w-28" type="submit">
             Resume{' '}
@@ -75,7 +75,7 @@ export default function AboutPage() {
             {/* map the socials */}
         </div>
         <h4 className="text-3xl">Work Experiences</h4>
-        <p className="mb-2 mt-0">Here's a brief rundown of my most recent work experiences.</p>
+        <p className="mb-2 mt-0">Here&apos;s a brief rundown of my most recent work experiences.</p>
         <div className="timeline mt-4 mb-6">
           <div className="relative pl-8 border-l border-gray-300 dark:border-gray-600">
             <div className="mb-6">
